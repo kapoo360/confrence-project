f5860a33e1175979b70298260c61543bff624f46
kapoo360 <az805555@gmail.com>
2024-07-29 00:28:38 +0300

    added admin page

---
+++ Admin page con/assets/js/chartsJS.js
@@ -0,0 +1,43 @@
+const ctx1 = document.getElementById("chart-1").getContext("2d");
+const myChart = new Chart(ctx1, {
+  type: "polarArea",
+  data: {
+    labels: ["Facebook", "Youtube", "Amazon"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [600, 800, 1000],
+        backgroundColor: [
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+  },
+});
+
+const ctx2 = document.getElementById("chart-2").getContext("2d");
+const myChart2 = new Chart(ctx2, {
+  type: "bar",
+  data: {
+    labels: ["Facebook", "Youtube", "Amazon"],
+    datasets: [
+      {
+        label: "Earning",
+        data: [600, 800, 1000],
+        backgroundColor: [
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+  },
+});
